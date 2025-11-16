2071b8ec7ac9febaa47f17b9807de5b97aede53a
Mouloud <mouloud.bellil@outlook.fr>
2025-11-16 09:17:01 +0100

    generate processed data

--- data_processing/etl_data.py
+++ data_processing/etl_data.py
@@ -14,11 +14,12 @@ from conf.config import BASE_DIR
 from utils.db_utils import create_mongo_connection
 from utils.logger import logger
 
-table_initial_attribs = ["timestamp", "siteId", "deviceType", "sensorType", "measure", "unit"]
-csv_path = Path(BASE_DIR/"data"/"raw"/"iot_data.csv")
+table_initial_attribs = ["timestamp", "name", "deviceType", "sensorType", "measure", "unit"]
+raw_data = BASE_DIR/"data"/"raw"
+processed_data = BASE_DIR/"data"/"processed"
 
 
-def extract(csv_path: Path, logger: Logger) -> pd.DataFrame:
+def extract(raw_data: Path, logger: Logger) -> pd.DataFrame:
     """This function extracts the information from mongodb and save it to a dataframe"""
               
     # connect to db
@@ -27,7 +28,13 @@ def extract(csv_path: Path, logger: Logger) -> pd.DataFrame:
         os._exit(1)
 
     try:
+        
+        
+        
+        
         df = pd.DataFrame(columns=table_initial_attribs)
+        
+        csv_path = raw_data/"iot_data.csv"
         
         if csv_path.exists():
             df = pd.read_csv(csv_path)
@@ -54,44 +61,47 @@ def extract(csv_path: Path, logger: Logger) -> pd.DataFrame:
 
 
 
-def load_to_csv(df: pd.DataFrame, csv_path: Path, logger: Logger) -> None:
+def load_to_csv(df: pd.DataFrame, raw_data: Path, logger: Logger) -> None:
     """summary"""
     
     try:
-        df.to_csv(csv_path, index=False)
+        df.to_csv(raw_data/"iot_data.csv", index=False)
             
     except Exception:
         logger.critical("load_to_json - ")
         os._exit(1)
         
 
-def transform(data: List, logger: Logger) -> pd.DataFrame:
+def transform(raw_data: Path, processed_data: Path, logger: Logger) -> pd.DataFrame:
     """summary"""
 
-    last_extract_date = datetime(1900, 1, 1)
-    
-    if data:
-        last_extract_date = datetime.fromisoformat(data[-1]["timestamp"])
-    
-    # connect to db
-    mongodb_collection = create_mongo_connection(params, logger)
-    if mongodb_collection == -1:
+    try:
+        df = pd.read_csv(raw_data/"iot_data.csv")
+        sites = pd.DataFrame(df["name"].unique(), columns=["name"])
+        devices = pd.DataFrame(df["deviceType"].unique(), columns=["deviceType"])
+        units = pd.DataFrame(df["unit"].unique(), columns=["unit"])
+        
+        def save_to_csv(table, table_name):
+            table.reset_index()
+            table.index.name = "id"
+            table.index += 1
+            table.to_csv(processed_data/f"{table_name}.csv")
+
+        save_to_csv(sites, "site")
+        save_to_csv(devices, "device")
+        save_to_csv(units, "unit")
+        
+        
+        sitedevice = df[["name", "deviceType"]].drop_duplicates(subset=["name", "deviceType"])
+        # sitedevice["name"] = sitedevice["name"].apply(lambda x: x)
+        
+        print(sitedevice)
+        print(sites)
+        
+    except Exception:
+        logger.exception("transform - ")
         os._exit(1)
     
-    docs = mongodb_collection.find({"timestamp": {"$gt": last_extract_date}})
-    
-    df = pd.DataFrame(columns=table_initial_attribs)
-    
-    for doc in docs:
-        doc["timestamp"] = doc["timestamp"].isoformat()
-        for key, value in doc["metadata"].items():
-            doc[key]= value
-        del  doc["metadata"]
-        del doc["_id"]
-        curr_df = pd.DataFrame.from_dict(doc, orient="index").T
-        df = pd.concat([df, curr_df], ignore_index=True)
-    
-    return df
     
     
 
@@ -104,9 +114,11 @@ if __name__ == "__main__":
     
     logger_ = logger("etl_iot.log", 1, 0)
 
-    df = extract(csv_path, logger_)
+    df = extract(raw_data, logger_)
+    
+    print(df)
 
-    load_to_csv(df, csv_path, logger)
+    load_to_csv(df, raw_data, logger_)
     
     
-    # load_to_json(new_data, logger_)
+    transform(raw_data, processed_data, logger_)
