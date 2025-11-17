efaae58a7bdd98fe63538ea2573d0f78c14b5ecf
Mouloud <mouloud.bellil@outlook.fr>
2025-11-17 21:06:56 +0100

    etl iot data ok

--- dag_iot.py
+++ dag_iot.py
@@ -1,18 +1,16 @@
-from datetime import datetime, timedelta
-from logging import Logger
-from pathlib import Path
 import sys
+from typing import List
 from airflow import DAG
+from logging import Logger
+from datetime import datetime, timedelta
 from airflow.providers.standard.operators.python import PythonOperator
 
 # add folder project to sys.path
-sys.path.append(
-    "/home/mouloud/Documents/projects/Smart_Irrigation_Optimization_Platform"
-)
+sys.path.append("/home/mouloud/Documents/projects/Smart_Irrigation_Optimization_Platform")
 
 from conf.config import BASE_DIR, config_params
-from data_processing.etl_iot import extract, load_to_csv, transform
-from utils.logger import echo_config, logger
+from data_processing.etl_iot import extract, load_to_postgre_db, transform
+from utils.logger import logger
 
 
 # run_time = datetime.now().strftime("%Y-%m-%d")
@@ -22,9 +20,6 @@ params["MONGO_COLLECTION"] = "sensor_data"
 log_file = BASE_DIR / "logs" / params["TODAY"] / f"{params["LOGGING_FILE"]}_ETL_IoT.log"
 logger_ = logger(log_file, params["CONSOLE_DEBUG_LEVEL"], params["FILE_DEBUG_LEVEL"])
 
-# logger_.info(f"Starding run, logfile => {log_file}")
-# echo_config(params, logger_)
-
 
 def extract_task_function(params: dict, logger: Logger) -> None:
     extract(params, logger)
@@ -32,11 +27,11 @@ def extract_task_function(params: dict, logger: Logger) -> None:
 
 def transform_task_function(params: dict, logger: Logger) -> None:
     transform(params, logger)
+    
 
+def load_task_function(tables: List, params: dict, logger: Logger) -> None:
+    load_to_postgre_db(tables, params, logger)
 
-# # # print(df)
-# load_to_csv(df, params, logger_)
-# transform(params, logger_)
 
 # default args
 default_args = {
@@ -69,12 +64,14 @@ transform_task = PythonOperator(
     dag=dag,
 )
 
-# load_task = PythonOperator(
-#     python_callable=load_to_csv,
-#     dag=dag,
-#     task_id="load_data_to_csv",
-#     op_kwargs={"df": extract(params, logger_), "params": params, "logger_": logger_},
-# )
+load_task = PythonOperator(
+    task_id="load_data_to_postgreSQL",
+    python_callable=load_task_function,
+    op_kwargs={"tables": ["sites", "devices", "sensors", "measures"], "params": params, "logger": logger_},
+    dag=dag,
+)
 
 # Define pipeline
-extract_task >> transform_task
+extract_task >> transform_task >> load_task
+
+load_to_postgre_db(["sites", "devices", "sensors", "measures"], params, logger_)
