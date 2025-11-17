4a54c4624d2041cd472656384749d0cb2648078f
Mouloud <mouloud.bellil@outlook.fr>
2025-11-17 22:04:27 +0100

    update parameters config

--- conf/config.py
+++ conf/config.py
@@ -1,3 +1,4 @@
+from datetime import date, datetime, time, timedelta
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -21,34 +22,48 @@ TP_API_PARAMS = {
 def config_params():
     """Retreive Common Config Pamameters in a dictionary format"""
 
-    prs = os.environ
+    params = os.environ
 
     return {
         # General parameters
-        "CONSOLE_DEBUG_LEVEL": int(prs["CONSOLE_DEBUG_LEVEL"]),
-        "FILE_DEBUG_LEVEL": int(prs["FILE_DEBUG_LEVEL"]),
-        "LOGGING_FILE": prs["LOGGING_FILE"],
-        "MAX_DOCS": int(prs.get("MAX_DOCS", 1)),
+        "CONSOLE_DEBUG_LEVEL": int(params.get("CONSOLE_DEBUG_LEVEL", "1")),
+        "FILE_DEBUG_LEVEL": int(params.get("FILE_DEBUG_LEVEL", "0")),
+        "LOGGING_FILE": params.get("LOGGING_FILE", "logger"),
+        "MAX_DOCS": int(params.get("MAX_DOCS", 1)),
+        
         # IoT parameters
-        "INPUT_DATA_FILE": prs.get("INPUT_DATA_FILE", ""),
-        "IoT_TOPIC": prs.get("IoT_TOPIC", ""),
-        "SITE_IDS": prs.get("SITE_IDS", 1),
+        "INPUT_DATA_FILE": params.get("INPUT_DATA_FILE", ""),
+        "IoT_TOPIC": params.get("IoT_TOPIC", ""),
+        "SITE_IDS": params.get("SITE_IDS", 1),
+        
         # API weather parameters
-        "WEATHER_TOPIC": prs.get("WEATHER_TOPIC", ""),
+        "WEATHER_TOPIC": params.get("WEATHER_TOPIC", ""),
+        
         # API NASA
-        "LATITUDE": float(prs.get("LATITUDE", 0.0)),
-        "LONGITUDE": float(prs.get("LONGITUDE", 0.0)),
-        "ST_TEMP_TOPIC": prs.get("ST_TEMP_TOPIC"),
+        "LATITUDE": float(params.get("LATITUDE", 0.0)),
+        "LONGITUDE": float(params.get("LONGITUDE", 0.0)),
+        "ST_TEMP_TOPIC": params.get("ST_TEMP_TOPIC"),
+        
         # API NDVI
-        "BBOX_COORDS": [float(x) for x in prs.get("BBOX_COORDS", "1.240,48.320,-1.150,48.380").split(",")],
-        "NDVI_TOPIC": prs.get("NDVI_TOPIC"),
+        "BBOX_COORDS": [float(x) for x in params.get("BBOX_COORDS", "1.240,48.320,-1.150,48.380").split(",")],
+        "NDVI_TOPIC": params.get("NDVI_TOPIC"),
+        
         # API Earth Engine
-        "START_DATE": prs.get("START_DATE"),
-        "END_DATE": prs.get("END_DATE"),
-        "ET_TOPIC": prs.get("ET_TOPIC"),
+        "START_DATE": params.get("START_DATE"),
+        "END_DATE": params.get("END_DATE"),
+        "ET_TOPIC": params.get("ET_TOPIC"),
+        
         # Mongodb parameters
-        "MONGO_USERNAME": prs["MONGO_USERNAME"],
-        "MONGO_PASSWORD": prs["MONGO_PASSWORD"],
-        "MONGO_DATABASE": prs["MONGO_DATABASE"],
-        "MONGO_COLLECTION": prs["MONGO_COLLECTION"],
+        "MONGO_USERNAME": params["MONGO_USERNAME"],
+        "MONGO_PASSWORD": params["MONGO_PASSWORD"],
+        "MONGO_DATABASE": "irrigation_db",
+        "MONGO_COLLECTION": params.get("MONGO_COLLECTION"),
+        
+        # Data folder
+        "RAW_DATA_PATH": BASE_DIR/"data"/"raw",
+        "PROCESSED_DATA": BASE_DIR/"data"/"processed",
+        
+        # ETL
+        "LAST_EXTRACT_DATE": datetime.combine(datetime.today().date() - timedelta(days=1), time(12, 0, 0)),
+        "TODAY": str(date.today())
     }
