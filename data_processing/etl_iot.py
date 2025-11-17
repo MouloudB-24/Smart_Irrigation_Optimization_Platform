98847ff89e44d8440d415d9cbf21db97d07460bb
Mouloud <mouloud.bellil@outlook.fr>
2025-11-17 22:02:47 +0100

    processed iot data

--- data_processing/etl_iot.py
+++ data_processing/etl_iot.py
@@ -1,26 +1,14 @@
 
-
-from datetime import datetime
-import json
 import os
 from logging import Logger
-from pathlib import Path
 from typing import List
 import pandas as pd
-from pprint import pprint
+
+from utils.db_utils import create_mongo_connection, create_postgre_connection
 
 
-from conf.config import BASE_DIR
-from utils.db_utils import create_mongo_connection
-from utils.logger import logger
-
-table_initial_attribs = ["timestamp", "name", "deviceType", "sensorType", "measure", "unit"]
-raw_data = BASE_DIR/"data"/"raw"
-processed_data = BASE_DIR/"data"/"processed"
-
-
-def extract(raw_data: Path, logger: Logger) -> pd.DataFrame:
-    """This function extracts the information from mongodb and save it to a dataframe"""
+def extract(params: dict, logger: Logger) -> pd.DataFrame:
+    """This function extracts the information from mongodb and save it to a csv file"""
               
     # connect to db
     mongodb_collection = create_mongo_connection(params, logger)
@@ -28,21 +16,11 @@ def extract(raw_data: Path, logger: Logger) -> pd.DataFrame:
         os._exit(1)
 
     try:
+        # Initialize df
+        df = pd.DataFrame(columns=["timestamp", "name", "deviceType", "sensorType", "measure", "unit"])
         
-        
-        
-        
-        df = pd.DataFrame(columns=table_initial_attribs)
-        
-        csv_path = raw_data/"iot_data.csv"
-        
-        if csv_path.exists():
-            df = pd.read_csv(csv_path)
-            last_extract_date = df["timestamp"][df.index[-1]]
-        else:
-            last_extract_date = datetime(1900, 1, 1)   
-                 
-        docs = mongodb_collection.find({"timestamp": {"$gt": last_extract_date}})
+        # retrieve data from last 24 hours
+        docs = mongodb_collection.find({"timestamp": {"$gt": params["LAST_EXTRACT_DATE"]}})
         
         for doc in docs:
             # flattening metadata
@@ -53,95 +31,94 @@ def extract(raw_data: Path, logger: Logger) -> pd.DataFrame:
             
             curr_df = pd.DataFrame.from_dict(doc, orient="index").T
             df = pd.concat([df, curr_df], ignore_index=True)
-        return df
+        
+        # Save data
+        df.to_csv(f"{params["RAW_DATA_PATH"]}/iot_data_{params["TODAY"]}.csv", index=False)        
+        logger.info("extract - IoT data has been extracted")
     
-    except Exception:
-        logger.critical("extract - ")
-        os._exit(1)
-
-
-
-def load_to_csv(df: pd.DataFrame, raw_data: Path, logger: Logger) -> None:
-    """summary"""
-    
-    try:
-        df.to_csv(raw_data/"iot_data.csv", index=False)
-            
-    except Exception:
-        logger.critical("load_to_json - ")
+    except Exception as e:
+        logger.critical(f"extract  - error when extracting IoT data: {e}")
         os._exit(1)
         
 
-def transform(raw_data: Path, processed_data: Path, logger: Logger) -> pd.DataFrame:
+def transform(params: dict, logger: Logger) -> pd.DataFrame:
     """summary"""
 
     try:
-        df = pd.read_csv(raw_data/"iot_data.csv")
+        df = pd.read_csv(f"{params['RAW_DATA_PATH']}/iot_data_{params["TODAY"]}.csv")
         
-        def save_to_csv(table: pd.DataFrame, table_name: str, index_name: str="", include_id: bool=True):
+        processed_data_iot = params["PROCESSED_DATA"]/"iot"
+        processed_data_iot.mkdir(exist_ok=True, parents=True)
+        
+        def save_to_csv(df: pd.DataFrame, table: str, index_name: str="", include_id: bool=True):
             if include_id:
-                table.reset_index(drop=True)
-                table.index += 1
-                table.index.name = index_name
-                table.to_csv(processed_data/f"{table_name}.csv", index=True)
+                df.reset_index(drop=True)
+                df.index += 1
+                df.index.name = index_name
+                df.to_csv(processed_data_iot/f"{table}_{params["TODAY"]}.csv", index=True)
             else:
-                table.to_csv(processed_data/f"{table_name}.csv", index=False)  
+                df.to_csv(processed_data_iot/f"{table}_{params["TODAY"]}.csv", index=False)  
     
         def search_id(df1, df2, col1, col2):
             return df1[col1].apply(lambda x: df2[df2[col2] == x].index[0])
         
-        # sites table
+        # sites data
         sites = pd.DataFrame(df["name"].unique(), columns=["site_name"])
         save_to_csv(sites, "sites", "site_id")
-        
-        # devices table
+    
+        # devices data
         devices = pd.DataFrame(df["deviceType"].unique(), columns=["device_type"])
         save_to_csv(devices, "devices", "device_id")
-        
-        # sites_devicess table (many to many)
-        sites_devices = df[["name", "deviceType"]].drop_duplicates(subset=["name", "deviceType"])
-        sites_devices["site_id"] = search_id(sites_devices, sites, "name", "site_name")
-        sites_devices["device_type"] = search_id(sites_devices, devices, "deviceType", "device_type")
-        sites_devices = sites_devices[["site_id", "device_type"]]
-        save_to_csv(sites_devices, "sites_devices", include_id=False)
-        
-        # sensors table
+    
+        # sensors data
         sensors = pd.DataFrame(df[["sensorType", "unit"]].drop_duplicates(subset=["sensorType", "unit"]))
         sensors.rename(columns=({"sensorType": "sensor_type", "unit": "sensor_unit"}), inplace=True)
         save_to_csv(sensors, "sensors", "sensor_id")
+
         
-        # devicesensor table (many to many)
-        devices_sensors = df[["deviceType", "sensorType"]].drop_duplicates(subset=["deviceType", "sensorType"])
-        devices_sensors["device_id"] = search_id(devices_sensors,devices,  "deviceType", "device_type")
-        devices_sensors["sensor_id"] = search_id(devices_sensors, sensors, "sensorType", "sensor_type")
-        devices_sensors = devices_sensors[["device_id", "sensor_id"]]
-        save_to_csv(devices_sensors, "devices_sensors", include_id=False)
-        
-        # measure table
+        # measure data
         measures = df
         measures["site_id"] = measures["name"].apply(lambda x: sites[sites["site_name"] == x].index[0])
-        measures["device_id"] = search_id(measures, devices, "deviceType", "device_type")
         measures["sensor_id"] = search_id(measures, sensors, "sensorType", "sensor_type")
-        measures = measures[["timestamp", "site_id", "device_id", "sensor_id", "measure"]]
+        measures = measures[["timestamp", "sensor_id", "measure"]]
         save_to_csv(measures, "measures", "measure_id")
+
+        logger.info("transform - IoT data bas been transformed")
         
     except Exception as e:
-        logger.exception(f"transform - unexpected error: {e}")
+        logger.exception(f"transform - error when transforming IoT data: {e}")
         os._exit(1)
+        
 
-if __name__ == "__main__":
-    params = {}
-    params["MONGO_USERNAME"]="root"
-    params["MONGO_PASSWORD"]="mongodbroot"
-    params["MONGO_DATABASE"]="irrigation_db"
-    params["MONGO_COLLECTION"]="sensor_data"
+def load_to_postgre_db(tables: List, params: dict, logger: Logger) -> None:
+    """summary"""
+    try:
+        params["POSTGRE_USER"]="postgres"
+        params["POSTGRE_PASSWORD"]="postgresroot"
+        params["POSTGRE_DATABASE"]="iot_db"
+        
+        engine = create_postgre_connection(params, logger)
+        if engine == -1:
+            os._exit(1)
     
-    logger_ = logger("etl_iot.log", 1, 0)
+        processed_data_iot = params["PROCESSED_DATA"]/"iot"
+        
+        
+        for table in tables:
+            df = pd.read_csv(processed_data_iot/f"{table}_{params["TODAY"]}.csv")
+            
+            print(f"Type de engine: {type(engine)}")
+            print(f"Engine: {engine}")
+            print(f"Tables à charger: {tables}")
+            print(f"Colonnes du DataFrame: {df.columns.tolist()}")
 
-    df = extract(raw_data, logger_)
+            df.to_sql(table, engine, if_exists="append", index=False)
+        
+            logger.info(f"load_to_postgre_db - IoT data has been loading: {table}")
+            
+    except Exception as e:
+        logger.critical(f"load_to_postgre_db - error when loading IoT data: {e}")
+        os._exit(1)
     
-
-    load_to_csv(df, raw_data, logger_)
-    
-    
-    transform(raw_data, processed_data, logger_)
+    finally:
+        engine.dispose()
